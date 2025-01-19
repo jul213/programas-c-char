7071337866c7673c122d76decedd9ea3d2bbcebd
jul213 <158296107+jul213@users.noreply.github.com>
2025-01-19 20:41:23 +0000

    uso de propiedades en main.cs

--- generador_contraseñass/main.cs
+++ generador_contraseñass/main.cs
@@ -6,6 +6,12 @@ public static void main(String []args){
     Console.Write("ingresa la longitud de contraseña");
     int longitud = int.Parse(Console.ReadLine());
 
+    Contraseña.UsarMayusculas = true;
+    Contraseña.UsarMinusculas = true;
+    Contraseña.UsarNumeros = true;
+    Contraseña.UsarSimbolos = false;
+    Contraseña.CaracteresObligatorios = "AB20";
+
     String contraseñaGenerada = contra.generarContraseña(longitud);
     Console.WriteLine($"la contraseña generada es: {contraseñaGenerada}");
 
