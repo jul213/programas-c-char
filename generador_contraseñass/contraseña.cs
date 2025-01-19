a4b3a0d92cad09d2de4627778c6d9070e2a443c4
jul213 <158296107+jul213@users.noreply.github.com>
2025-01-19 10:49:53 +0000

    añadir fuente de aleatoridad mas segura

--- generador_contraseñass/contraseña.cs
+++ generador_contraseñass/contraseña.cs
@@ -1,21 +1,36 @@
 namespace contra;
 using System;
+using System.Security.Cryptography;
 public static Contraseña {
 
     public String generarContraseña(int longitud){
         
         String contraseña = new StringBuilder(longitud);
+        if (longitud<=0){
+
+            throw new ArgumentException("la longitud tiene ser mayor que 0");
+        } else { 
         String[] caracteres = {"a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "ñ", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z",
                                 "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z",
                                 "0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "_", "-", "+", "=", "[", "]", "{", "}", ";", ":", "<", ">", "|", ".", "?"}
 
-        Random eleccion = new Random();
+        
+        using (const rng = new RNGCryptoServiceProveider){ 
+        
 
+        Byte[] randomNumber= new Byte[1]; 
         for (i=0;i<longitud;i++){
-            int indice = eleccion.Next(0,caracteres.Length);
-            contraseña.Append(caracteres[indice.Next(caracteres.Length)]);
+            rng.GetBytes(randomNumber);
+            int indice = randomNumber[0] % caracteres.Length;
+            contraseña.Append(caracteres[indice]);
+
+         }
 
         }
         return contraseña.ToString;
+
+       
+
+      }
     }
 }
