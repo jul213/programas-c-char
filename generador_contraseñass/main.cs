922a396487006b212c21c4f8f3761021d6b1d253
jul213 <158296107+jul213@users.noreply.github.com>
2025-01-19 10:34:56 +0000

    uso en main.cs

--- generador_contraseñass/main.cs
+++ generador_contraseñass/main.cs
@@ -4,4 +4,9 @@ public static void main(String []args){
     Console.WriteLine("bienvenido al generador de contraseñas");
     Console.Write("ingresa la longitud de contraseña");
     int longitud = int.Parse(Console.ReadLine());
+
+    String contraseñaGenerada = contra.generarContraseña(longitud);
+    Console.WriteLine($"la contraseña generada es: {contraseñaGenerada}");
+
+    Console.ReadKey();
 }
