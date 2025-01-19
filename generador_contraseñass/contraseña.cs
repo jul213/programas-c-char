a5963c7477f11e35c95338217a2fa3211aefc356
jul213 <158296107+jul213@users.noreply.github.com>
2025-01-19 20:09:52 +0000

    terminar propiedades

--- generador_contraseñass/contraseña.cs
+++ generador_contraseñass/contraseña.cs
@@ -5,15 +5,18 @@ public static Contraseña {
 
     public static bool UsarMayusculas {get; set;} = true;
     public static bool UsarMinusculas {get; set;} = true;
+    public static bool UsarNumeros {get;set;} = true;
+    public static bool UsarSimbolos {get;set,} = true;
+    public static string CaracteresObligatorios {get; set;} = ""; 
 
-    public String generarContraseña(int longitud){
+    public string generarContraseña(int longitud){
         
-        String contraseña = new StringBuilder(longitud);
+        string contraseña = new StringBuilder(longitud);
         if (longitud<=0){
 
             throw new ArgumentException("la longitud tiene ser mayor que 0");
         } else { 
-        String[] caracteres = {"a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "ñ", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z",
+        string[] caracteres = {"a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "ñ", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z",
                                 "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z",
                                 "0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "_", "-", "+", "=", "[", "]", "{", "}", ";", ":", "<", ">", "|", ".", "?"}
 
