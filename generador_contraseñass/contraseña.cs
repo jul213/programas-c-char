1124dabca118680848f9993c852ecb3e8cf7c426
jul213 <158296107+jul213@users.noreply.github.com>
2025-01-19 10:40:29 +0000

    uso de stringBuilder para mejorar rendimiento

--- generador_contraseñass/contraseña.cs
+++ generador_contraseñass/contraseña.cs
@@ -4,7 +4,7 @@ public static Contraseña {
 
     public String generarContraseña(int longitud){
         
-        String contraseña = String.Empty;
+        String contraseña = new StringBuilder(longitud);
         String[] caracteres = {"a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "ñ", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z",
                                 "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z",
                                 "0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "_", "-", "+", "=", "[", "]", "{", "}", ";", ":", "<", ">", "|", ".", "?"}
@@ -13,9 +13,9 @@ public static Contraseña {
 
         for (i=0;i<longitud;i++){
             int indice = eleccion.Next(0,caracteres.Length);
-            contraseña += caracteres[indice];
+            contraseña.Append(caracteres[indice.Next(caracteres.Length)]);
 
         }
-        return contraseña;
+        return contraseña.ToString;
     }
 }
