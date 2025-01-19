303f91f2b24e0e82b16119b81c9143e8a5e6cd8d
jul213 <158296107+jul213@users.noreply.github.com>
2025-01-19 20:44:01 +0000

    class Program

--- generador_contraseñass/main.cs
+++ generador_contraseñass/main.cs
@@ -1,19 +1,21 @@
 using contra;
 using System;
 
-public static void main(String []args){
-    Console.WriteLine("bienvenido al generador de contraseñas");
-    Console.Write("ingresa la longitud de contraseña");
-    int longitud = int.Parse(Console.ReadLine());
+class Program { 
+    public static void main(String []args){
+        Console.WriteLine("bienvenido al generador de contraseñas");
+        Console.Write("ingresa la longitud de contraseña");
+        int longitud = int.Parse(Console.ReadLine());
 
-    Contraseña.UsarMayusculas = true;
-    Contraseña.UsarMinusculas = true;
-    Contraseña.UsarNumeros = true;
-    Contraseña.UsarSimbolos = false;
-    Contraseña.CaracteresObligatorios = "AB20";
+        Contraseña.UsarMayusculas = true;
+        Contraseña.UsarMinusculas = true;
+        Contraseña.UsarNumeros = true;
+        Contraseña.UsarSimbolos = false;
+        Contraseña.CaracteresObligatorios = "AB20";
 
-    String contraseñaGenerada = contra.generarContraseña(longitud);
-    Console.WriteLine($"la contraseña generada es: {contraseñaGenerada}");
+        String contraseñaGenerada = contra.generarContraseña(longitud);
+        Console.WriteLine($"la contraseña generada es: {contraseñaGenerada}");
 
-    Console.ReadKey();
+        Console.ReadKey();
+    }
 }
