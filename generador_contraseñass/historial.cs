dbd7c4c9ce395931fe97a329ea50226dfc63117f
jul213 <158296107+jul213@users.noreply.github.com>
2025-01-20 09:30:31 +0000

    crear metodo AgregarContraseña

--- generador_contraseñass/historial.cs
+++ generador_contraseñass/historial.cs
@@ -5,7 +5,13 @@ class Historial {
 
   public static historial(var contraseña){
 
-    
+    private static List<string> historial = new List<string>;
+
+    public static void AgregarContraseña(string contraseña){
+
+      historial.Add(contraseña);
+    }
+
   }
 
 }
